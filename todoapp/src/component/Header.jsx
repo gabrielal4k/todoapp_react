b7aa49d495df447d38624f71e100d3f2886f37f4
Gab <gabriel_al4k@yahoo.com>
2025-06-20 10:35:58 +0800

    styling form

--- todoapp/src/component/Header.jsx
+++ todoapp/src/component/Header.jsx
@@ -2,8 +2,8 @@ import styles from './header.module.css';
 
 export default function Header(){
     return (
-        <div >
-            <h1 className={styles.header}>Todo App</h1>
+        <div className={styles.header}>
+            <h1 >Todo App</h1>
             <p>Keep track of your tasks</p>
         </div>
     );
